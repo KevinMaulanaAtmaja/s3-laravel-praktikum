4af5ed13e50d9a9731765d3ace33a697a0f41680
Kevin Maulana Atmaja <kevinmaulanaat@gmail.com>
2025-09-01 12:00:00 +0700

    routing

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,33 @@ Route::get('/', function () {
 Route::get('/test', function () {
     return "Test";
 });
+
+Route::get('/hello', function () {
+    echo "Hello World";
+});
+
+// task
+Route::view('/profile', 'profile', ['name' => 'Kevin Maulana']);
+
+Route::redirect('/mahasiswa', '/profile');
+
+Route::get('profile/{nim}', function ($nim) {
+    return "Nim " . $nim;
+})->where('nim', '[0-9]+');
+
+// explore
+Route::get('user/{name?}', function ($name = "Kevin") {
+    return $name;
+});
+
+Route::get('/tes-route', function () {
+    $route = Route::current();              // object route sekarang
+    $name = Route::currentRouteName();      // nama route
+    $action = Route::currentRouteAction();  // controller@method
+
+    return [
+        'uri' => $route->uri(),
+        'name' => $name,
+        'action' => $action,
+    ];
+})->name('tes.route');
